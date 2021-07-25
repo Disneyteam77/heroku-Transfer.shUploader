ce63a2548ed56c0d591d59c835011329b176dc5c
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-07-25 14:16:47 +0530

    idk

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ async def start(event):
             "Dev":"https://t.me/AmiFutami",
             "Source code":"https://github.com/Achu2234/heroku-Transfer.shUploader"}
     buttons = [[Button.url(k, v)] for k,v in dict_.items()]
-+
+
     await event.respond('Hi!\nMy Name Is Transfer Uploader Bot Sent any file or direct download link to upload and get the transfer.sh download link Bot Made by @AmiFutami', buttons=buttons)
     raise events.StopPropagation
 
