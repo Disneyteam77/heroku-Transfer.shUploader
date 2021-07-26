a2bb6160dda4ba52cd730c2d0aafe54dad7c6971
Disneyteam <85325455+Disneyteam76@users.noreply.github.com>
2021-07-26 12:20:50 +0530

    Update

--- bot.py
+++ bot.py
@@ -20,19 +20,19 @@ bot = TelegramClient('Uploader bot', api_id, api_hash).start(bot_token=bot_token
 async def start(event):
     """Send a message when the command /start is issued."""
     dict_ = {
-            "Channel":"https://t.me/AsmSafone",
-            "Support":"https://t.me/safothebot",
-            "Dev":"https://t.me/AmiFutami",
-            "Source code":"https://github.com/Achu2234/heroku-Transfer.shUploader"}
+            "Updates Channel":"https://t.me/disneygrou",
+            "Support Group":"https://t.me/disneyteamchat",
+            "Developer":"https://t.me/Doreamonfans2",
+            "Backup Channel":"https://t.me/disneygroubackup"}
     buttons = [[Button.url(k, v)] for k,v in dict_.items()]
 
-    await event.respond('Hi!\nMy Name Is Transfer Uploader Bot Sent any file or direct download link to upload and get the transfer.sh download link Bot Made by @AmiFutami', buttons=buttons)
+    await event.respond('Hi!\nMy Name Is Disney Team Transfer Uploader Bot Send me any file or direct download link and I upload and get the transfer.sh download link Bot Made by ❤ In 🇮🇳India by [Doreamonfans](https://t.me/doreamonfans2)', buttons=buttons)
     raise events.StopPropagation
 
 @bot.on(events.NewMessage)
 async def echo(update):
     """Echo the user message."""
-    msg = await update.respond("Processing...")
+    msg = await update.respond("Processing Plz Wait😁...")
     
     try:
         if not os.path.isdir(download_path):
@@ -41,7 +41,7 @@ async def echo(update):
         start = time.time()
         
         if not update.message.message.startswith("/") and not update.message.message.startswith("http") and update.message.media:
-            await msg.edit("**Downloading start...**")
+            await msg.edit("**Downloading starting😉...**")
             file_path = await bot.download_media(update.message, download_path, progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
                 progress(d, t, msg, start)))
         else:
@@ -51,10 +51,10 @@ async def echo(update):
             
         print(f"file downloaded to {file_path}")
         try:
-            await msg.edit("Download finish!\n\n**Now uploading...**")
+            await msg.edit("Download finish!\n\n**Now uploading plz wait😍...**")
             download_link, final_date, size = await send_to_transfersh_async(file_path, msg)
             name = os.path.basename(file_path)
-            await msg.edit(f"**Name: **`{name}`\n**Size:** `{size}`\n**Link:** {download_link}")
+            await msg.edit(f"**Name: **`{name}`\n**Size:** `{size}`\n**Link:** {download_link} /n**Owner:** [Doreamonfans1](https://t.me/doreamonfans1)")
         except Exception as e:
             print(e)
             await msg.edit(f"Uploading Failed\n\n**Error:** {e}")
@@ -63,11 +63,11 @@ async def echo(update):
             print("Deleted file :", file_path)
     except Exception as e:
         print(e)
-        await msg.edit(f"Download link is invalid or not accessable\n\n**Error:** {e}")
+        await msg.edit(f"Download link is invalid or not accessable contact my [owner](https://t.me/doreamonfans1)\n\n**Error:** {e}")
 
 def main():
     """Start the bot."""
-    print("\nBot started...\n")
+    print("\nBot started visit @disneygrou For more updates...\n")
     bot.run_until_disconnected()
 
 if __name__ == '__main__':
